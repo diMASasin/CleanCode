dd307ecd1b8d293a03c9fc5a048734128a5a00f2
diMASasin <35368058+diMASasin@users.noreply.github.com>
2024-07-22 20:35:33 +0500

    Update CleanCode_ExampleTask11.cs

--- CleanCode/CleanCode_ExampleTask11.cs
+++ CleanCode/CleanCode_ExampleTask11.cs
@@ -1,8 +1,3 @@
-namespace MethodName;
-
-public class CleanCode_ExampleTask11
-{
-    int armySize = 10;
-    int coinsNumber = 10;
-    string name = "Vladislav";
-}
+int armySize = 10;
+int coinsNumber = 10;
+string name = "Vladislav";
